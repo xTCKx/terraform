3524b7a1743679003e88f488548f0d5f6aea272e
xTCKx <czoet31@gmail.com>
2025-09-08 13:47:30 +0000

    2

--- main.tf
+++ main.tf
@@ -7,25 +7,58 @@ terraform {
 }
 
 provider "esxi" {
-  esxi_hostname = "192.168.1.7"
-  esxi_hostport = "22"
+  esxi_hostname = var.esxi_hostname
+  esxi_hostport = var.esxi_hostport
   esxi_hostssl  = "443"
   esxi_username = "root"
   esxi_password = "Welkom01!"
 }
 
-resource "esxi_guest" "vmtest" {
-  guest_name = "vmtest"
+resource "esxi_guest" "webserver" {
+  count      = 2
+  guest_name = "webserver+${count.index + 1}"
   disk_store = "datastore1"
-  memsize    = 1024
+  memsize    = 2048
   numvcpus   = 1
-
 
   ovf_source = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.ova"
   network_interfaces {
     virtual_network = "VM Network"
   }
+
+  guestinfo = {
+    "metadata"          = filebase64("metadata.yaml")
+    "metadata.encoding" = "base64"
+    "userdata"          = filebase64("userdata.yaml")
+    "userdata.encoding" = "base64"
+  }
+
 }
+
+resource "esxi_guest" "databaseserver" {
+  guest_name = "databaseserver"
+  disk_store = "datastore1"
+  memsize    = 2048
+  numvcpus   = 1
+
+  ovf_source = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.ova"
+  network_interfaces {
+    virtual_network = "VM Network"
+  }
+
+  guestinfo = {
+    "metadata"          = filebase64("metadata.yaml")
+    "metadata.encoding" = "base64"
+    "userdata"          = filebase64("userdata.yaml")
+    "userdata.encoding" = "base64"
+  }
+
+
+}
+
+
+
+
 
 
 ##guestinfo base64
